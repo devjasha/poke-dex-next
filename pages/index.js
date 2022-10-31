51507537818ee012e91ffb11e2a7d64aae22547c
jasha.chec <jasha@chec.de>
2022-10-31 18:04:35 +0100

    feat(m-stage): import stage

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,14 @@
 import Navigation from "../components/navigation";
-
+import Stage from "../components/stage";
 const Home = () => {
   return (
     <div>
       <header>
         <Navigation />
       </header>
+      <main>
+        <Stage />
+      </main>
     </div>
   );
 };
