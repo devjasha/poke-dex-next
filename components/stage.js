b598f93ac866260072f62d0ae22fcf8423939ac9
jasha.chec <jasha@chec.de>
2022-11-01 08:32:43 +0100

    feat(stage): add stage

---
+++ components/stage.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+import Image from "next/image";
+
+const Stage = () => {
+  return (
+    <section className="h-screen w-full flex items-center justify-center">
+      <h1>Hello and Welcome, to the new Pokedex</h1>
+    </section>
+  );
+};
+
+export default Stage;
