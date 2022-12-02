883cd07f2e9ddd9d6e92a25f237d54baabac9f47
jasha.chec <jasha@chec.de>
2022-12-02 19:48:58 +0100

    fix(Stage): redesign Stage

--- app/Stage.js
+++ app/Stage.js
@@ -1,16 +1,14 @@
 import Link from "next/link";
 import Image from "next/image";
-import PokemonSearch from "./PokemonSearch";
 
 const Stage = () => {
   return (
-    <section className="h-[80vh] w-full flex items-center pl-9 pr-9">
+    <section className="h-[40vh] items-end w-full flex pl-9 pr-9 mb-7">
       <div className="lg:max-w-7xl mx-auto">
-        <h1 className="text-4xl lg:text-7xl mb-6">
+        <h1 className="text-4xl lg:text-7xl">
           Welcome,
           <br /> to the new Pokedex
         </h1>
-        <PokemonSearch />
       </div>
     </section>
   );
