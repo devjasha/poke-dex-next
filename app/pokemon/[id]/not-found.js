818ebbd9f8f65f5b2939487f1e648bf4aa1056fe
jasha.chec <jasha@chec.de>
2022-12-02 22:11:39 +0100

    feat(not-found): style go back button

--- app/pokemon/[id]/not-found.js
+++ app/pokemon/[id]/not-found.js
@@ -1,10 +1,16 @@
 import Link from "next/link";
+import { AiOutlineArrowLeft } from "react-icons/ai";
 
 const NotFound = () => {
   return (
-    <div className="h-screen flex items-center justify-center">
-      Whoop it seems there is no entry for this Pokemon
-      <Link href="/">Go back</Link>
+    <div className="h-screen flex items-center justify-center flex-col">
+      <p> Whoop it seems there is no entry for this Pokemon</p>
+      <Link
+        className="py-3 px-6 w-auto my-10 rounded-md text-white dark:text-black bg-black dark:bg-white flex items-center"
+        href="/"
+      >
+        <AiOutlineArrowLeft className="mr-3" /> Go back!
+      </Link>
     </div>
   );
 };
