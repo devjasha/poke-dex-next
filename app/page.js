495c1c2d48041625925aae7ce873582a60050b33
jasha.chec <jasha@chec.de>
2022-12-02 19:50:50 +0100

    fix(page): add api call and new module

--- app/page.js
+++ app/page.js
@@ -1,11 +1,36 @@
 import Stage from "./Stage";
-import PokemonList from "./PokemonList";
+import SearchWithPokemon from "./SearchWithPokemon";
+import axios from "axios";
+import { use } from "react";
+
+const getData = async () => {
+  const data = await axios
+    .get("https://pokeapi.co/api/v2/pokemon?limit=10000&offset=0")
+    .then(async (response) => {
+      return response.data;
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+
+  return data;
+};
+
+const data = getData();
 
 const Home = () => {
+  const pokemonData = use(data);
+
+  const formattedPokemonData = Object.entries(pokemonData.results).map(
+    (entry) => ({
+      pokemon_name: entry[1].name,
+      pokemon_url: entry[1].url,
+    })
+  );
   return (
     <div>
       <Stage />
-      <PokemonList />
+      <SearchWithPokemon apiData={formattedPokemonData} />
     </div>
   );
 };
