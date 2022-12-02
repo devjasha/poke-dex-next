593a9969d1adbe05831ac168b3b28a7ad0378185
jasha.chec <jasha@chec.de>
2022-12-02 22:14:35 +0100

    fix(page): edit text

--- app/about/page.js
+++ app/about/page.js
@@ -8,8 +8,8 @@ const About = () => {
         <div className="mt-7">
           <p className="w-auto max-w-xl">
             Hello, my name is Jasha Chec, I am a developer trainee from Hamburg.
-            At the moment I&apos;m working at JungVonMatt. I built this project
-            for a variety of reasons. On the one hand, of course, for my
+            At the moment I&apos;m working at Jung von Matt. I built this
+            project for a variety of reasons. On the one hand, of course, for my
             personal development, on the other hand, to provide the Pokemon
             community with a clear and easy-to-use information page. The project
             will of course continue to be developed further. Have fun with the
