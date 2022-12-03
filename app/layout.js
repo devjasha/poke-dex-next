43580e5f41bcd03dcbc1ef409f7114d6a14bf8ba
jasha.chec <jasha@chec.de>
2022-12-03 13:09:00 +0100

    feat(seo): add new meta tags

--- app/layout.js
+++ app/layout.js
@@ -7,6 +7,16 @@ export default function RootLayout({ children }) {
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta charSet="UTF-8" />
+        <meta
+          name="description"
+          content="Explore the world of Pokemon with the new Pokedex"
+        />
+        <meta
+          name="keywords"
+          content="Pokedex, Pokemon, pokewiki, pokemon, pokedex, Pokewiki"
+        />
+        <meta name="robots" content="max-image-preview:[large]" />
+        <meta property="og:image" content="/images/pokedex-preview.png" />
         <title>Pokedex</title>
       </head>
       <body>
