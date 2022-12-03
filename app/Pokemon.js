ab22de2f346b08d99c534ec1680030a79df6336f
jasha.chec <jasha@chec.de>
2022-12-03 13:51:33 +0100

    fix(Pokemon): change Link

--- app/Pokemon.js
+++ app/Pokemon.js
@@ -10,24 +10,24 @@ const Pokemon = ({
   pokemonNumber,
 }) => {
   return (
-    <div className="rounded-xl backdrop-blur-md shadow-[rgba(0,_0,_0,_0.24)_0px_3px_8px] my-4 lg:mx-4 p-6 overflow-x-hidden">
-      <div className="relative ">
-        <TbPokeball className="absolute top-2/4 left-6 -translate-x-2/4 -translate-y-2/4 w-80 h-80 text-slate-200 -z-10" />
-        <Image
-          src={pokemonImage}
-          height={300}
-          width={300}
-          alt={pokemonName}
-          loading="lazy"
-        />
-      </div>
-      <div>
-        <h3 className="text-2xl uppercase mb-3 relative">{pokemonName}</h3>
-        <Link href={`/pokemon/${pokemonDetails}`} className="hover:underline">
+    <Link href={`/pokemon/${pokemonDetails}`} className="hover:underline">
+      <div className="rounded-xl backdrop-blur-md shadow-[rgba(0,_0,_0,_0.24)_0px_3px_8px] my-4 lg:mx-4 p-6 overflow-x-hidden">
+        <div className="relative ">
+          <TbPokeball className="absolute top-2/4 left-6 -translate-x-2/4 -translate-y-2/4 w-80 h-80 text-slate-200 -z-10" />
+          <Image
+            src={pokemonImage}
+            height={300}
+            width={300}
+            alt={pokemonName}
+            loading="lazy"
+          />
+        </div>
+        <div>
+          <h3 className="text-2xl uppercase mb-3 relative">{pokemonName}</h3>
           More details <HiChevronRight className="inline" />
-        </Link>
+        </div>
       </div>
-    </div>
+    </Link>
   );
 };
 
