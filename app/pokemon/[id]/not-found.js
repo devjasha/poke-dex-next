ccd7abf245a90edfab169c8b3ee6870dc2e20259
jasha.chec <jasha@chec.de>
2023-01-09 16:40:17 +0100

    feat(not-fount): add gif

--- app/pokemon/[id]/not-found.js
+++ app/pokemon/[id]/not-found.js
@@ -4,6 +4,7 @@ import { AiOutlineArrowLeft } from "react-icons/ai";
 const NotFound = () => {
   return (
     <div className="h-screen flex items-center justify-center flex-col">
+      <iframe src="https://giphy.com/embed/SsjyaYH2gkNCE" width="480" height="360" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>
       <p> Whoop it seems there is no entry for this Pokemon</p>
       <Link
         className="py-3 px-6 w-auto my-10 rounded-md text-white dark:text-black bg-black dark:bg-white flex items-center"
@@ -12,7 +13,7 @@ const NotFound = () => {
         <AiOutlineArrowLeft className="mr-3" /> Go back!
       </Link>
     </div>
-  );
+  )
 };
 
 export default NotFound;
