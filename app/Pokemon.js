b5a5ff580bb6be4874addc12af972cc510a403bd
jasha.chec <jasha@chec.de>
2023-06-20 10:22:07 +0200

    fix(Pokemon): remove hover effect

--- app/Pokemon.js
+++ app/Pokemon.js
@@ -34,7 +34,7 @@ const Pokemon = ({
   };
 
   return (
-    <Link href={`/pokemon/${pokemonDetails}`} className="hover:underline">
+    <Link href={`/pokemon/${pokemonDetails}`}>
       <div className="rounded-xl backdrop-blur-md shadow-[rgba(0,_0,_0,_0.24)_0px_3px_8px] my-4 lg:mx-4 p-6 overflow-x-hidden">
         <div className="relative ">
           <TbPokeball className="absolute top-2/4 left-6 -translate-x-2/4 -translate-y-2/4 w-80 h-80 text-slate-200 -z-10" />
