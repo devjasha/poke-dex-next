c31df971ec78598b70f527c54b891e091c98cb60
jasha.chec <jasha@chec.de>
2023-06-20 10:22:19 +0200

    feat(loading): add loading animation

--- app/loading.js
+++ app/loading.js
@@ -1,7 +1,27 @@
-export default function Loading() {
+"use client";
+
+import { motion } from "framer-motion";
+
+const Loading = () => {
   return (
-    <div className="h-screen w-full flex items-center justify-center flex-col">
-      <h1>Loading ...</h1>
+    <div className="h-screen w-full flex items-center justify-center">
+      <motion.div
+        className="h-[200px] w-[200px] bg-black"
+        animate={{
+          scale: [1, 2, 2, 1, 1],
+          rotate: [0, 0, 180, 180, 0],
+          borderRadius: ["0%", "0%", "50%", "50%", "0%"],
+        }}
+        transition={{
+          duration: 2,
+          ease: "easeInOut",
+          times: [0, 0.2, 0.5, 0.8, 1],
+          repeat: Infinity,
+          repeatDelay: 1,
+        }}
+      />
     </div>
-  )
-}
+  );
+};
+
+export default Loading;
